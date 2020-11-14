a8ebfef439995347875a301d6540f518c23502f3
Senya <formobileprima@gmail.com>
2020-11-15 01:28:16 +0200

    g

---
+++ lesson_6_lists/index.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# print(list('Hello'))  #['H', 'e', 'l', 'l', 'o']
+
+# c = [c * 3 for c in "list"] #['lll', 'iii', 'sss', 'ttt']
+# v = [c * 3 for c in "list"] #['lll', 'iii', 'sss', 'ttt']
+# print(v)
+
+
+# arr = [c + d for c in 'Hello' if c!='o' for d in 'world!' if d != 'l']
+# # ['Hw', 'Ho', 'Hr', 'Hd', 'H!', 'ew', 'eo', 'er', 'ed', 'e!', 'lw', 'lo', 'lr', 'ld', 'l!', 'lw', 'lo', 'lr', 'ld', 'l!']
+# print(arr)
+
+arr = [1,2,3]
+# arr.append(4) # [1, 2, 3, 4]
+# arr.append('L') # [1, 2, 3, 'L']
+# arr.insert(1,'X') # [1, 'X', 2, 3]
+
+# arr = [1,1,2,2,3,3]
+# arr.remove(2) # [1, 1, 2, 3, 3]
+
+# item = arr.pop() # [1, 2]
+# print(item) # 3
+
+arr = [1,2,2,3,3,3,4,4,4,4]
+print(arr.count(3)) # 3. We calculate counte mated items in array
